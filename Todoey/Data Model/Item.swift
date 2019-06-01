e12e65168c66c5584a9f66d9bb8d8cc273b4c54d
karlaustriainocencio <karlinocencio@icloud.com>
2019-06-01 21:10:25 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Karl Inocencio on 28/5/19.
+//  Copyright © 2019 Karl Inocencio. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
